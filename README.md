a94419fd18b082e302e011c0472eb99ec608e110
augustine-uba1 <augustine_ubanzemeka@yahoo.com>
2023-07-01 23:28:38 +0100

    feat : added pydantic for user entry validation to post requests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fastApi
 creating a fast API REST API application
+
+uvicorn main:app --reload
+pip install fastapi[all]
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Posts(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -11,7 +18,14 @@ async def root():
 def feeds():
     return {"Feeds" : "Here is a list of feeds"}
 
-@app.post("/createfeed")
-def create_feed(payload: dict = Body(...)):
-    print(payload)
-    return{"new feed": f"feed title: {payload['title']}, content: {payload['content']}"}
+# getting both parameters without pydantic
+# @app.posts("/createfeed")
+# def create_feed(payload: dict = Body(...)):
+#     print(payload)
+#     return{"new feed": f"feed title: {payload['title']}, content: {payload['content']}"}
+
+@app.post("/createposts")
+def create_posts(posts : Posts):
+    print(posts.rating, posts.published)
+    print(posts.dict())
+    return{"data": posts}
