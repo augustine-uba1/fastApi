2f33a7a7cfe7217776b857d10ec1d7da9699754b
augustine-uba1 <augustine_ubanzemeka@yahoo.com>
2023-07-02 09:45:04 +0100

    feat: added the following routes; get post by id, create post. Also added validation to the route parameters, and updated status codes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
-
+from random import randrange
 app = FastAPI()
 
 class Posts(BaseModel):
@@ -10,22 +10,35 @@ class Posts(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "title of post 2", "content": "content of post 2", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to my api!!!"}
 
-@app.get("/feeds")
-def feeds():
-    return {"Feeds" : "Here is a list of feeds"}
+@app.get("/posts")
+def get_posts():
+    return {"data" : my_posts}
 
-# getting both parameters without pydantic
-# @app.posts("/createfeed")
-# def create_feed(payload: dict = Body(...)):
-#     print(payload)
-#     return{"new feed": f"feed title: {payload['title']}, content: {payload['content']}"}
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post : Posts):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(100, 10000000)
+    my_posts.append(post_dict)
+    print(my_posts)
+    return{"data": post_dict}
 
-@app.post("/createposts")
-def create_posts(posts : Posts):
-    print(posts.rating, posts.published)
-    print(posts.dict())
-    return{"data": posts}
+@app.get("/post/{id}")
+def get_post(id : int):
+    
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id {id} was not found")
+    return{"post detail": post}
