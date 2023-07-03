bd6d19e384cbc2a14ebd86212b69eca80552a1d3
augustine-uba1 <augustine_ubanzemeka@yahoo.com>
2023-07-03 17:38:29 +0100

    feat: added delete and put routes

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ def find_post(id):
         if p['id'] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+    
 @app.get("/")
 async def root():
     return {"message": "Welcome to my api!!!"}
@@ -42,3 +47,28 @@ def get_post(id : int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id {id} was not found")
     return{"post detail": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id : int):
+    index = find_index_post(id)
+    
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id {id} does not exist")
+    
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Posts):
+    index = find_index_post(id)
+    
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id {id} does not exist")
+    
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    print (my_posts)
+    return {"data" : post_dict}
