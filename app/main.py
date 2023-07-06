add7330ceef49db8fae98e1f7feae0619d8f244e
augustine-uba1 <augustine_ubanzemeka@yahoo.com>
2023-07-06 06:45:59 +0100

    added ORM model:moved connection class to database file

--- app/main.py
+++ app/main.py
@@ -7,20 +7,11 @@ import psycopg
 import time
 
 import models
-from database import engine, SessionLocal
-
+from database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 class Posts(BaseModel):
     title: str
